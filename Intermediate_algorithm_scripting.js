a35106d013ce644b61a9aab5fb3884c050dea6b1
Bernard Pagoaga <bernard.pagoaga@gmail.com>
2016-05-25 09:58:07 +0200

    Update Intermediate_algorithm_scripting.js

--- Intermediate_algorithm_scripting.js
+++ Intermediate_algorithm_scripting.js
@@ -71,3 +71,151 @@ function convertHTML(str) {
 
 convertHTML("Dolce & Gabbana");
 
+
+
+
+/*============================
+Sorted Union
+============================*/
+
+
+function uniteUnique(arr) {
+  var newArray = [];
+  var args = Array.from(arguments);
+  
+  args.forEach(function(element){
+    //var elem = Array.from(element);
+    
+    for(var i=0; i<element.length; i++){
+      
+      if (newArray.indexOf(element[i]) === -1){
+        
+        newArray.push(element[i]);
+      }
+    }
+  });
+  
+ 
+  return newArray;
+}
+
+uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+
+
+
+
+/*===========================
+Spinal Tap
+===========================*/
+
+
+function spinalCase(str) {
+  // "It's such a fine line between stupid, and clever."
+  // --David St. Hubbins
+  
+  // replacing spaces and underscores by dashes
+  str = str.replace(/[\s_]/g, "-");
+  
+  // looping trough the string to replace Uppercase chars
+  for (var i=0; i<str.length; i++){
+    
+    var pos = str.charAt(i),
+        beforePos = str.charAt(i-1); // this var will be used to test if we have for example spinalTap and replace it with spinal-tap
+    
+    if ( (/[A-Z]/).test(pos) ){
+      str = str.replace(pos, pos.toLowerCase()); // ok, we replace uppercase by lowercase
+      
+      if ( (/[a-zA-Z]/).test(beforePos) ){ // if an uppercase letter is preceded by a letter instead of a space or an underscore, we had a dash
+        var arr = str.split("");
+        arr[i-1] = arr[i-1]+"-";
+        str = arr.join(""); // let me know if there is a better way to do this
+        
+      
+      }
+    }
+    
+    
+    
+  }
+  return str;
+}
+
+spinalCase('thisIsSpinalTap');
+
+
+
+
+/*===============================
+Sum All Odd Fibonacci
+===============================*/
+
+
+
+function sumFibs(num) {
+  
+      var odd_arr = [],
+      sum = 0;
+  
+  // fibonacci
+  var arr = fibonacci(num);
+  
+  // pushing the odd values
+  arr.forEach(function(value){
+    if( value % 2 ){
+      odd_arr.push(value);
+    }
+  });
+  
+  sum = odd_arr.reduce(function(previous, current){
+    return previous + current;
+  });
+  
+  return sum;
+  
+}
+
+function fibonacci(num){
+  var arr = [1,1];
+  
+  for (var i = 0; (arr[i]+arr[i+1]) <= num; i++){
+    arr[i+2] = arr[i+1] + arr[i];
+  }
+  console.log(arr);
+  return arr;
+}
+
+sumFibs(4);
+
+
+
+
+/*===================
+Sum all Primes
+===================*/
+
+
+function sumPrimes(num) {
+  var not_primes =[],
+      primes = [];
+  
+  for (var i = 2; i<= num; i++){
+    
+    for (var j =1; j<=i; j++){
+      if( i%j ===0 && i!==j && j!==1 && not_primes.indexOf(i) === -1){
+        not_primes.push(i);
+      }
+    }
+    
+    if( not_primes.indexOf(i) === -1 ){
+      primes.push(i);
+    }
+  }
+  
+  var result = primes.reduce(function(prev,curr){
+    return prev+curr;
+  });
+  
+  return result;
+}
+
+sumPrimes(977);
