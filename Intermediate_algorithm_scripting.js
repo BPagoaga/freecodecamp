ea2326fcd891c88f95e41f37b8cb87ed2ce30707
Bernard Pagoaga <bernard.pagoaga@gmail.com>
2016-06-01 23:35:06 +0200

    Update Intermediate_algorithm_scripting.js

--- Intermediate_algorithm_scripting.js
+++ Intermediate_algorithm_scripting.js
@@ -350,3 +350,24 @@ function steamrollArray(arr) {
 }
 
 steamrollArray([1, [2], [3, [[4]]]]);
+
+
+
+
+
+/*=======================
+Binary Agent
+=======================*/
+
+
+function binaryAgent(str) {
+  var arr = str.split(' ');
+  
+  arr.forEach(function(i,index){
+    arr[index] = String.fromCharCode(parseInt(i,2));
+  });
+    
+  return arr.join("");
+}
+
+binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
