f96c9c1a894b919d864667e1aef1165531a9496c
Bernard Pagoaga <bernard.pagoaga@gmail.com>
2016-06-03 13:47:05 +0200

    Update advanced_algorithm_scripting.js

--- advanced_algorithm_scripting.js
+++ advanced_algorithm_scripting.js
@@ -6,30 +6,17 @@ Validate US Phone Numbers
 
 function telephoneCheck(str) {
   // Good luck!
-  
-  //case 1 : a country code is provided
-  // we check that it matches the country, then remove it
-  // cases : "1 ", "1-", "1)", "1555"
-  if( (/^1[-\s(]/).test(str) ){
-    str = str.substr(1);
+  // numbers can't start with a dash, this could be included in the regexp but complicate it way too much imo
+  if( (/^-/).test(str) ){
+    return false;
   }
-  
   // clearing whitespaces and dashes for better testing
   str = str.replace(/[\s-]/gi, "");
   
-  //case 2 : no country code
-  // testing the area code
-  // two cases : 555 and (555)
-  if( (/^\d{3}/).test(str) ){
-    str = str.substr(3);
-  }else if( (/^\(\d{3}\)/).test(str) ){
-    str = str.substr(5);
-  }else{
-    return false;
-  }
-  
-  //testing the remaining string : it must have 7 numbers
-  if( (/^\d{7}$/).test(str) ){
+  // the number 1 can be match 0 or 1 time (country code)
+  // the area code is 555 or (555)
+  // and finally we have 7 digits
+  if( (/^1?(\d{3}|\(\d{3}\))\d{7}$/).test(str) ){
     return true;
   }else{
     return false;
