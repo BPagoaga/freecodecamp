394428cd593395bb63272a8c023eb3e93054ebcc
Bernard Pagoaga <bernard.pagoaga@gmail.com>
2016-06-08 10:06:04 +0200

    Update advanced_algorithm_scripting.js

--- advanced_algorithm_scripting.js
+++ advanced_algorithm_scripting.js
@@ -25,3 +25,60 @@ function telephoneCheck(str) {
 }
 
 telephoneCheck("155-555-5555");
+
+
+
+
+
+/*==========================
+Symetric Difference
+==========================*/
+
+
+function sym(args) {
+  
+  args = Array.from(arguments);
+  for(var k=0;k<args.length; k++){
+    args[k] = cleanArray(args[k]);
+  }
+  
+  var arr = args.reduce(function(a,b){
+    return a.concat(b);
+  });
+  
+  
+  
+  var counts = {};
+
+  for(var i = 0; i< arr.length; i++) {
+      var num = arr[i];
+      counts[num] = counts[num] ? counts[num]+1 : 1;
+  }
+  
+  arr = [];
+  
+  for(var prop in counts){
+    if(args.length<=2 && counts[prop] === 1){
+      arr.push(parseInt(prop));
+    }else if(args.length>2 && (counts[prop] === 1 || counts[prop] === 3)){
+      arr.push(parseInt(prop));
+    }
+  }
+  
+  return arr;
+}
+
+//cleanArray removes all duplicated elements
+function cleanArray(array) {
+  var i, j, len = array.length, out = [], obj = {};
+  for (i = 0; i < len; i++) {
+    obj[array[i]] = 0;
+  }
+  for (j in obj) {
+    out.push(parseInt(j));
+  }
+  return out;
+}
+
+sym([1, 1, 2, 5], [2, 2, 3, 5], [3, 4, 5, 5]);
+
