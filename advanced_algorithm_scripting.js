17ecd8bfa25cc7c582a08f788c9578e6f1e4b76b
Bernard Pagoaga <bernard.pagoaga@gmail.com>
2016-06-23 23:17:08 +0200

    Update advanced_algorithm_scripting.js

--- advanced_algorithm_scripting.js
+++ advanced_algorithm_scripting.js
@@ -383,3 +383,34 @@ var bob = new Person('Bob Ross');
 bob.getFullName();
 
 
+
+
+/*==============
+Map the Debris
+==============*/
+
+
+function orbitalPeriod(arr) {
+  var GM = 398600.4418;
+  var earthRadius = 6367.4447;
+  var newArr = [];
+  
+  arr.forEach(function(object){
+    
+    var a = (earthRadius + object.avgAlt),
+        base = Math.pow(a, 3)/GM,
+        orbitalPeriod = Math.round(2*Math.PI*Math.sqrt(base));
+    
+    object = {
+      name: object.name,
+      orbitalPeriod: orbitalPeriod
+    };
+    
+    newArr.push(object);
+  }); 
+  
+  return newArr;
+}
+
+orbitalPeriod([{name: "iss", avgAlt: 413.6}, {name: "hubble", avgAlt: 556.7}, {name: "moon", avgAlt: 378632.553}]);
+
