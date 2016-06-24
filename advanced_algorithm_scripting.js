220976b62d7d4bae0e4c284c6e53f77983d8ee25
Bernard Pagoaga <bernard.pagoaga@gmail.com>
2016-06-24 15:55:39 +0200

    Update advanced_algorithm_scripting.js

--- advanced_algorithm_scripting.js
+++ advanced_algorithm_scripting.js
@@ -414,3 +414,40 @@ function orbitalPeriod(arr) {
 
 orbitalPeriod([{name: "iss", avgAlt: 413.6}, {name: "hubble", avgAlt: 556.7}, {name: "moon", avgAlt: 378632.553}]);
 
+
+
+
+/*=====================
+Pairwise
+=====================*/
+
+
+
+function pairwise(arr, arg) {
+  
+  // never trust user input
+  if (arr.length === 0 || typeof arg !== 'number') { 
+    return 0; 
+  }
+  
+  var result=[];
+  
+  for (var i = 0; i < arr.length; i++) {      
+    for (var j = 0; j < arr.length; j++) {  
+      if (
+        i !== j && 
+        arr[i]+arr[j] === arg &&
+        result.indexOf(i) < 0 && result.indexOf(j) < 0
+        ) {                      
+        result.push(i,j);
+        break; // the lower !
+      }
+    }
+  }
+  
+  return result.reduce(function(a,b){
+    return a+b;
+  });
+}
+
+pairwise([1,4,2,3,0,5], 7);
